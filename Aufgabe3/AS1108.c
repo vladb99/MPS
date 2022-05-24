2ea59cc5efd033d8f0c3f63605b4bc95bac56289
unknown <vl161bra@fhkn.de>
2022-05-24 10:28:33 +0200

    Prepare Aufgabe3

---
+++ Aufgabe3/AS1108.c
@@ -0,0 +1,156 @@
+/*
+ * SPI_AS1108.c
+ *
+ *  Created on: 26.12.2018
+ *      Author: Admin
+ */
+
+
+#include <msp430.h>
+#include "../base.h"
+#include "event.h"
+#include "AS1108.h"
+
+// Basis des Zahlensystems
+// Einstellung zwischen 2 und 10 soll m�glich sein
+#define BASE 10
+
+// fifth element is a dont care
+signed char digits[] = {0, 0, 0, 0, 0};
+UChar selected;
+UChar i;
+
+// es sind geeignete Datenstrukturen f�r den Datenaustausch
+// zwischen den Handlern festzulegen.
+
+LOCAL Void AS1108_Write(UChar adr, UChar arg) {
+   Char ch = UCA1RXBUF;   // dummy read, UCRXIFG := 0, UCOE := 0
+   CLRBIT(P2OUT,  BIT3);  // Select aktivieren
+   UCA1TXBUF = adr;       // Adresse ausgeben
+   while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0);
+   ch = UCA1RXBUF;        // dummy read
+   UCA1TXBUF = arg;       // Datum ausgeben
+   while (TSTBIT(UCA1IFG, UCRXIFG) EQ 0);
+   //ch = UCA1RXBUF;        // dummy read
+   SETBIT(P2OUT,  BIT3);  // Select deaktivieren
+}
+
+
+GLOBAL Void SPI_Init(Void) {
+   // set up Pin 3 at Port 2 => CS output, High
+   SETBIT(P2OUT,  BIT3);
+   SETBIT(P2DIR,  BIT3);
+   CLRBIT(P2SEL0, BIT3);
+   CLRBIT(P2SEL1, BIT3);
+   CLRBIT(P2REN,  BIT3);
+
+   // set up Pins 4, 5 and 6 at Port 2
+   CLRBIT(P2SEL0, BIT4 + BIT5 + BIT6);
+   SETBIT(P2SEL1, BIT4 + BIT5 + BIT6);
+
+   // set up Universal Serial Communication Interface A
+   SETBIT(UCA1CTLW0, UCSWRST);        // UCA1 software reset
+
+   // in �bereinstimung mit dem SPI-Timing-Diagramm von AS1108
+   UCA1CTLW0_H = 0b10101001;          // clock phase: rising edge, ...
+                                      // ... clock polarity: inactive low ...
+                                      // ... MSB first, 8-bit data, SPI master mode, ...
+                                      // ... 3-pin SPI, synchronus mode
+
+   CLRBIT(UCA1CTLW0, UCSSEL1);        // select ACLK ...
+   SETBIT(UCA1CTLW0, UCSSEL0);        // .. as source clock
+   UCA1BRW = 0;                       // prescaler
+
+   CLRBIT(UCA1CTLW0, UCSWRST);        // release the UCA0 for operation
+}
+
+
+// der Treiberbaustein AS1108 ist hier �ber die SPI-Schnittstelle zu initialisieren
+GLOBAL Void AS1108_Init(Void) {
+    // Shutdown
+    AS1108_Write(0x0C, 0x81);
+    // Decode mode on Code-B for all digits
+    AS1108_Write(0x09, 0xFF);
+    // Intensity on max
+    AS1108_Write(0x0A, 0x06);
+    // Scan limit on display all digits
+    AS1108_Write(0x0B, 0x03);
+
+    set_event(EVENT_DIGI);
+}
+
+// ----------------------------------------------------------------------------
+// der Button-Handler beinhaltet keine Zustandsmaschiene
+GLOBAL Void Button_Handler(Void) {
+    if (tst_event(EVENT_BTN3)) {
+        clr_event(EVENT_BTN3);
+        selected = 0;
+        set_event(EVENT_15);
+    } else if (tst_event(EVENT_BTN4)) {
+        clr_event(EVENT_BTN4);
+        selected = 1;
+        set_event(EVENT_15);
+    } else if (tst_event(EVENT_BTN5)) {
+        clr_event(EVENT_BTN5);
+        selected = 2;
+        set_event(EVENT_15);
+    } else if (tst_event(EVENT_BTN6)) {
+        clr_event(EVENT_BTN6);
+        selected = 3;
+        set_event(EVENT_15);
+    }
+}
+
+// ----------------------------------------------------------------------------
+// der Number-Handler beinhaltet keine Zustandsmaschiene
+GLOBAL Void Number_Handler(Void) {
+    if (tst_event(EVENT_15)) {
+       clr_event(EVENT_15);
+       digits[selected] += 1;
+       if (TSTBIT(P2OUT, BIT7)) {
+           digits[selected] -= 2;
+       }
+       set_event(EVENT_DIGI);
+   }
+}
+
+// ----------------------------------------------------------------------------
+// Datentyp eines konstanten Funktionspointers
+typedef Void (* VoidFunc)(Void);
+
+// Funktionsprototypen
+LOCAL Void State0(Void);
+LOCAL Void State1(Void);
+
+// lokale Zustandsvariable
+LOCAL VoidFunc state = State0;
+
+LOCAL Void State0(Void) {
+    if (tst_event(EVENT_DIGI)) {
+        i = 1;
+        state = State1;
+    }
+}
+
+LOCAL Void State1(Void) {
+    if (i <= 4) {
+        UChar index = i - 1;
+        if (digits[index] > BASE-1) {
+            digits[index] = 0;
+            digits[i]++;
+        } else if (digits[index] < 0) {
+            digits[index] = BASE-1;
+            digits[i]--;
+        }
+        AS1108_Write(i, digits[index]);
+        i++;
+    } else {
+        clr_event(EVENT_DIGI);
+        state = State0;
+    }
+}
+
+// der AS1108_Hander beinhaltet eine Zustandsmaschine
+GLOBAL Void AS1108_Handler(Void) {
+    (*state)();
+}
---
+++ Aufgabe3/TA0.c
@@ -0,0 +1,163 @@
+/*
+ * TA0.c
+ *
+ *  Created on: 22.04.2018
+ *      Author: Admin
+ */
+
+#include <msp430.h>
+#include "../base.h"
+#include "TA0.h"
+#include "event.h"
+#include "GPIO.h"
+
+#define CNTMAX 4
+// 1/800Hz * 1000 = 1.25ms
+// 250ms / 1.25ms = 200
+#define BTNMAX 6
+#define TIMER_COUNT 200
+
+/*
+ * Man soll sich eine geeignete Datenstruktur �berlegen,
+ * die eine laufzeiteffiziente Ausf�hrung der ISR erm�glicht.
+ */
+
+typedef struct {
+    const UChar * const port;
+    const UChar mask;
+    const TEvent msg;
+    UInt * const pcnt; // Hysterese Wert,
+} TButton;
+
+LOCAL struct {
+    UInt idx;
+    UInt cnt[BTNMAX];
+    const TButton *ptr;
+} button;
+
+LOCAL const TButton btn[BTNMAX] = {
+    {(UChar *)(&P1IN), BIT0, EVENT_BTN1, &button.cnt[0]},
+    {(UChar *)(&P1IN), BIT1, EVENT_BTN2, &button.cnt[1]},
+    {(UChar *)(&P3IN), BIT0, EVENT_BTN3, &button.cnt[2]},
+    {(UChar *)(&P3IN), BIT1, EVENT_BTN4, &button.cnt[3]},
+    {(UChar *)(&P3IN), BIT2, EVENT_BTN5, &button.cnt[4]},
+    {(UChar *)(&P3IN), BIT3, EVENT_BTN6, &button.cnt[5]}
+};
+
+// 10ms Schritte
+LOCAL const UChar blink_musters[] = {
+        8, 2, 0,
+        3, 3, 0,
+        1, 1, 0,
+        2, 8, 0,
+        2, 2, 2, 8, 0,
+        2, 2, 2, 2, 2, 8, 0
+};
+
+LOCAL const UChar * const map[] = {
+    &blink_musters[0],
+    &blink_musters[3],
+    &blink_musters[6],
+    &blink_musters[9],
+    &blink_musters[12],
+    &blink_musters[17]
+};
+
+static UChar step_count;
+static UChar array_index;
+static UChar cnt_led;
+static UChar pattern_index;
+static UChar current_pattern_value;
+static UChar pattern_index_new;
+//static const struct Button btn1 = {P1IN, BIT1, EVENT_BTN1};
+
+GLOBAL Void set_blink_muster(UInt arg) {
+/*
+ * Die Funktion muss so erweitert werden,
+ * dass ein Blinkmuster selektiert wird.
+ * Diese L�sung h�ngt stark von der gew�hlten
+ * Datenstruktur ab.
+ */
+    pattern_index_new = arg;
+}
+
+// Der Timer A0 ist bereits initialisiert
+GLOBAL Void TA0_Init(Void) {
+    button.idx = 0;
+    button.ptr = &btn[0];
+
+    for (UChar i=0; i LE BTNMAX-1; i++) {
+        button.cnt[i] = 0;
+    }
+
+    step_count = TIMER_COUNT;
+    array_index = 0;
+    cnt_led = 0;
+    pattern_index = 0;
+    current_pattern_value = 0;
+    pattern_index_new = 0;
+
+    //int *cheat_x = btn1->portid;
+    //*cheat_x = P1IN;
+    //btn1.portid = P1IN;
+
+    // Folie 27
+    TA0CCR0 = 0;                              // stopp Timer A
+    CLRBIT(TA0CTL, TAIFG);                    // clear overflow flag
+    CLRBIT(TA0CCR0, CCIFG);                   // clear CCI flag
+    TA0CTL  = TASSEL__ACLK + MC__UP + ID__8;  // set up Timer A
+    TA0EX0  = TAIDEX_7;                       // set up expansion register
+    // 9600 / 800Hz = 12
+    TA0CCR0 = 12;                           // set up CCR0 for 10 ms
+    SETBIT(TA0CTL, TACLR);                    // clear and start Timer
+    SETBIT(TA0CCTL0, CCIE);                   // enable Timer A interrupt
+
+    // Set LED2 once
+    SETBIT(P1OUT, BIT2);
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt Void TA0_ISR(Void) {
+    //static volatile UChar test = 0;
+    // Timer logic for 250ms step
+    // 1532 -> 1414
+    if (--step_count EQ 0) {
+        //test = *(*(P + pattern_index) + array_index);
+        step_count = TIMER_COUNT;
+        current_pattern_value = *(*(map + pattern_index) + array_index);
+        cnt_led++;
+
+        // LED2 Flash logic
+        if (cnt_led EQ current_pattern_value) {
+            TGLBIT(P1OUT, BIT2);
+            array_index++;
+            cnt_led = 0;
+            if (*(*(map + pattern_index) + array_index) EQ 0) {
+                array_index = 0;
+                pattern_index = pattern_index_new;
+            }
+        }
+    }
+
+    // Debouncing
+    register UInt * const pcnt = button.ptr->pcnt;
+    if (TSTBIT(*button.ptr->port, button.ptr->mask)) {
+        if (*pcnt LT CNTMAX) {
+            if  (++(*pcnt) EQ CNTMAX) {
+                set_event(button.ptr->msg);
+                __low_power_mode_off_on_exit();
+            }
+        }
+    } else {
+        if (*pcnt GT 0) {
+            (*pcnt)--;
+        }
+    }
+
+    button.ptr++;
+    button.idx++;
+    if (button.idx GT BTNMAX-1) {
+        button.idx = 0;
+        button.ptr = &btn[0];
+    }
+}
